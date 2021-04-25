20281b13ae15d03919bfdb1a71f43ac7a82f2d12
Gabriel Cirstea <gabriel_cirstea@protonmail.com>
2021-04-25 21:49:06 +0300

    ex7

---
+++ Tema3/EX7.pl
@@ -0,0 +1,17 @@
+:- op(500, fx, ~).
+:- op(700, xfy, user:([^,->,v,<->, are,+])).
+
+%disjunctia 
+% p v q = p->(q->p)
+%conjunctia
+% p ^ q = ~(p->~q)
+%echivalenta
+% p <-> q = (p->q) ^ (q->p) = ~((p->q)->~(q->p))
+
+ conectprim(V,V) :- atom(V), !.
+ conectprim(~X, ~Z):- conectprim(X,Z), !.
+ conectprim(X v Y,~ Z -> W) :- write("sau "), conectprim(X,Z), conectprim(Y,W), !.
+ conectprim(X^Y,~(Z -> (~W))) :- conectprim(X,Z), conectprim(Y,W), !.
+ conectprim(X->Y,Z->T) :- conectprim(X,Z), conectprim(Y,T), !.
+ conectprim(X<->Y,~((Z -> T) -> (~(T -> Z)))) :- conectprim(X,Z), 
+ 												conectprim(Y,T), !.
